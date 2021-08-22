1cfb58599a0d586b038c8ee7e11a14d83c3ceb87
Seung-a-oh <osa0215@naver.com>
2021-08-23 03:13:47 +0900

    csv 파일 생성하고 mobius에 업로드하기

--- mobius/csv_upload.py
+++ mobius/csv_upload.py
@@ -19,7 +19,7 @@ fpy.write("a = " + str(b))
 
 PPG = 4
 BEAT = 5
-NUM = 20
+NUM = 100
 
 count = 0
 
@@ -29,59 +29,59 @@ mcp_beat = ms.set_mcp(BEAT)
 fcsv.write('time,ppg,beat'+'\n')
 
 def ReadValue():
-	global count
+   global count
 
-	ppg_val = ms.readAnalog(mcp_ppg, PPG)
-	beat_val = ms.readAnalog(mcp_beat, BEAT)
-	
-	if beat_val > 3:
-		beat_val = 3.3
-	else:
-		beat_val = 0
+   ppg_val = ms.readAnalog(mcp_ppg, PPG)
+   beat_val = ms.readAnalog(mcp_beat, BEAT)
+   
+   if beat_val > 3:
+      beat_val = 3.3
+   else:
+      beat_val = 0
 
-	time = datetime.now().strftime('%y%m%d_%H:%M:%S.%f')[:-3]
+   time = datetime.now().strftime('%y%m%d_%H%M%S.%f')[:-3]
 
-	fcsv.write(str(time) + ',' + str(ppg_val) + ',' + str(beat_val)+'\n')	
-	count += 1
+   fcsv.write(str(time) + ',' + str(ppg_val) + ',' + str(beat_val)+'\n')   
+   count += 1
 
-	# threading
-	timer = threading.Timer(0.06, ReadValue)
-	timer.start()
+   # threading
+   timer = threading.Timer(0.05, ReadValue)
+   timer.start()
 
-	if count == NUM:
-		timer.cancel()
+   if count == NUM:
+      timer.cancel()
 
-		fcsv.close()
-		fpy.close()
+      fcsv.close()
+      fpy.close()
 
-		post('ppg')
-		post('beat')
-		# post('ecg', 'ecg')
+      post('ppg')
+      post('beat')
+      # post('ecg', 'ecg')
 
 def post(value):
-	df = pd.read_csv("../csv/PPG_"+filename+".csv", encoding="UTF-8")
-	
-	t = df.loc[:NUM, 'time']
-	va = t[0] + "," + t[NUM-1]
+   df = pd.read_csv("../csv/PPG_"+filename+".csv", encoding="UTF-8")
+   
+   t = df.loc[:NUM, 'time']
+   va = t[0] + "," + t[NUM-1]
 
-	v = df.loc[:NUM, value]
-	for i in range(NUM):
-		va = va + "," + str(v[i])
+   v = df.loc[:NUM, value]
+   for i in range(NUM):
+      va = va + "," + str(v[i])
 
-	url = "http://203.253.128.177:7579/Mobius/healthcare-interworking/ppg/" + value
+   url = "http://203.253.128.177:7579/Mobius/healthcare-interworking/ppg/" + value
 
-	cin_contents = va
+   cin_contents = va
 
-	payload = "{\n    \"m2m:cin\": {\n        \"con\": \"" + str(cin_contents) + "\"\n    }\n}"
-	headers = {
-	'Accept': 'application/json',
-	'X-M2M-RI': '12345',
-	'X-M2M-Origin': '{{aei}}',
-	'Content-Type': 'application/vnd.onem2m-res+json; ty=4'
-	}
+   payload = "{\n    \"m2m:cin\": {\n        \"con\": \"" + str(cin_contents) + "\"\n    }\n}"
+   headers = {
+   'Accept': 'application/json',
+   'X-M2M-RI': '12345',
+   'X-M2M-Origin': '{{aei}}',
+   'Content-Type': 'application/vnd.onem2m-res+json; ty=4'
+   }
 
-	response = requests.request("POST", url, headers=headers, data=payload)
+   response = requests.request("POST", url, headers=headers, data=payload)
 
-	print(response.text)
+   print(response.text)
 
 ReadValue()
