d5bb517d2ee1d31c1e2a563cdd5e47935388d055
Seung-a-oh <osa0215@naver.com>
2021-08-23 03:14:08 +0900

    mobius에서 데이터 가져와서 csv 파일 생성하기

--- mobius/get_data.py
+++ mobius/get_data.py
@@ -3,8 +3,7 @@ from datetime import datetime
 import time
 import json
 
-fcsv = open("./from_mobius.csv", "w")
-
+#응답 전역변수
 json_response = ""
 
 def get(value):
@@ -18,30 +17,35 @@ def get(value):
     'X-M2M-RI': '12345',
     'X-M2M-Origin': 'SOrigin'
   }
-
+  
   response = requests.request("GET", url, headers=headers, data=payload)
   json_response = json.loads(response.text)
   print(response.text)
 
 slicing = ""
+
 def making(value):
   global slicing
-  fcsv.write('time,'+value+'\n')
-  slicing = json_response['m2m:cin']['con'].split(',')
-  # start = slicing[0][7:17]
-  # stop = slicing[1][7:17]
-  start = 203322.777
-  stop = 203323.998
-  range = float(stop) - float(start)
-  step = round(range / (len(slicing)-2),3)
-  print(step)
 
-  for i in slicing[2:]:
+  fcsv = open("./from_mobius_"+value+".csv", "w")
+  fcsv.write('time,'+value+'\n')
+  
+  #텍스트 -> csv
+  slicing = json_response['m2m:cin']['con'].split(',')
+  start = slicing[0][7:17]  #시간 추출
+  stop = slicing[1][7:17]
+  range = float(그만) - float(start)  #interval 구하기
+  step = round(range / (len(slicing)-2),3) 
+
+  for i in slicing[2:]:  #csv 파일 작성
     fcsv.write(str(start) + ',' + i + '\n')
-    start = start + step
+    start = float(start) + step
+
+  print(step)
   fcsv.close()
+
+get('ppg')
+making('ppg')
 
 get('beat')
 making('beat')
-
-# json_response["con"]
